3e4d4a7620a5e4d976daaefdf8716a5ef80f5c9a
Owen Marett <owenmarett@gmail.com>
2022-03-01 14:17:53 -0500

    Add pokemon colors

--- colorAnalysis/pokeColorFinder.py
+++ colorAnalysis/pokeColorFinder.py
@@ -11,7 +11,17 @@ with open("../pokemondb/data/pokemon_data.json", "r") as f:
 
 for file in os.listdir(img_directory):
     full_path = os.path.join(img_directory, file)
+    key = file.split(".")[0]
 
+    if not key:
+        continue
+    
     colors = colorAnalyzer.get_hex_codes_for_img(path=full_path)
 
-    
+    try:
+        pokemon_data[key]["colors"] = colors
+    except KeyError:
+        pass
+
+with open("../pokemondb/data/pokemon_data_colors.json", "w") as f:
+    json.dump(pokemon_data, f)
